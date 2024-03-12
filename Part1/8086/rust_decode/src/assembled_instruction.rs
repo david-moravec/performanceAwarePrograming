b0709a36482de956b7326fec4016c62acf8cfd9e
David Moravec <moravec@numsolution.cz>
2024-03-12 13:56:12 +0100

    Add assembled instruction struct

--- Part1/8086/rust_decode/src/assembled_instruction.rs
+++ Part1/8086/rust_decode/src/assembled_instruction.rs
@@ -1,24 +1,73 @@
 use crate::instruction::instruction::Operation;
 
-type BitField = (u8, usize);
-
-struct InstructionBits {
-    opcode: BitField,
-    reg: Option<BitField>,
-    rm: Option<BitField>,
-    mode: Option<BitField>,
-    disp_lo: Option<BitField>,
-    disp_hi: Option<BitField>,
-    data_lo: Option<BitField>,
-    data_hi: Option<BitField>,
-    s: Option<BitField>,
-    w: Option<BitField>,
-    d: Option<BitField>,
-    v: Option<BitField>,
-    z: Option<BitField>,
+enum BitUsage {
+    LITERAL,
+    MOD,
+    REG,
+    RM,
+    W,
+    S,
+    D,
+    V,
+    Z,
+    DATALO,
+    DATAHI,
+    DISPLO,
+    DISPHI,
 }
 
-struct AssembledInstruction {
+struct Bits {
+    usage: BitUsage,
+    size: u8,
+    value: Option<u8>,
+}
+
+impl Bits {
+    pub const fn literal(value: u8) -> Self {
+        Bits {
+            usage: BitUsage::LITERAL,
+            value: Some(value),
+            size: 8 - value.leading_zeros() as u8,
+        }
+    }
+}
+
+const MOD: Bits = Bits {
+    usage: BitUsage::MOD,
+    size: 2,
+    value: None,
+};
+
+const REG: Bits = Bits {
+    usage: BitUsage::REG,
+    size: 3,
+    value: None,
+};
+
+const RM: Bits = Bits {
+    usage: BitUsage::RM,
+    size: 3,
+    value: None,
+};
+
+const D: Bits = Bits {
+    usage: BitUsage::D,
+    size: 1,
+    value: None,
+};
+
+const W: Bits = Bits {
+    usage: BitUsage::W,
+    size: 1,
+    value: None,
+};
+
+pub struct AssembledInstruction {
     operation: Operation,
-    bits: InstructionBits,
+    bits: [Bits; 6],
 }
+
+pub const INSTRUCTION_TABLE: [AssembledInstruction; 1] = [AssembledInstruction {
+    operation: Operation::MOV,
+    bits: [Bits::literal(0b100010), D, W, MOD, REG, RM],
+}];
