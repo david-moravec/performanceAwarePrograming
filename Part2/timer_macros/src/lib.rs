69eae09ce3b07c564a9a17d6c64300c7a292eb96
David Moravec <moravec@numsolution.cz>
2024-09-11 14:24:17 +0200

    Add time_block macro

--- Part2/timer_macros/src/lib.rs
+++ Part2/timer_macros/src/lib.rs
@@ -1,2 +1,15 @@
 pub use timer::{print_timer, TIMER};
 pub use timer_internal::time_it;
+
+#[macro_export]
+macro_rules! time_block {
+    ($ident: expr, $block: block) => {
+        unsafe {
+            ::timer_macros::TIMER.start($ident);
+        }
+        $block;
+        unsafe {
+            ::timer_macros::TIMER.stop($ident);
+        }
+    };
+}
